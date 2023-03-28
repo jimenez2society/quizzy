bd2376090df3e2e23868c337de60d67f50707f0c
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-03-28 17:44:24 -0600

    created a Question class whit a randomizeLetter method to randomize the correct answer

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-
-
 // path and javascript for /index.html
+
+import { Question } from "./Question.js";
+
 // redirect to home path
 if (window.location.pathname === "/") {
   window.location.pathname = "/index.html";
@@ -15,10 +16,15 @@ if (window.location.pathname === "/index.html") {
   });
 }
 if (window.location.pathname === "/questions.html") {
+  let question1 = new Question("How long is this test?", [
+    "10 questions",
+    "20 questions",
+    "30 questions",
+  ]);
+
   // let isOnQuestion = beginTimer();
 
   // start timer
-  
 
   // display question
 
@@ -28,7 +34,6 @@ if (window.location.pathname === "/questions.html") {
 
   // move to next question and reset timer
 
-  
   Array.from(document.querySelectorAll(".question-choice")).map((choice) => {
     choice.addEventListener("click", (e) => {
       let choiceInput = Array.from(choice.children).filter(
