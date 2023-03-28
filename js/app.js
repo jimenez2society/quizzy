2f1d2fcfebe5a5f7f2e139b1868669fa72d454f4
jimenez2society <121840097+jimenez2society@users.noreply.github.com>
2023-03-28 16:21:54 -0600

    finished the questions container styles and is now ready to have javascript logic implemented. Commented in the app.js file to prepare for the question container logic

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
-import { beginTimer } from "./timer.js";
 
+
+// path and javascript for /index.html
+// redirect to home path
+if (window.location.pathname === "/") {
+  window.location.pathname = "/index.html";
+}
 if (window.location.pathname === "/index.html") {
   document.querySelector(".begin").addEventListener("click", (e) => {
     document.querySelector(".welcome-screen").classList.add("fade");
@@ -10,7 +15,20 @@ if (window.location.pathname === "/index.html") {
   });
 }
 if (window.location.pathname === "/questions.html") {
-  beginTimer();
+  // let isOnQuestion = beginTimer();
+
+  // start timer
+  
+
+  // display question
+
+  // handle if timer expires
+
+  // submit chosen answer and add it to the currentTestScore
+
+  // move to next question and reset timer
+
+  
   Array.from(document.querySelectorAll(".question-choice")).map((choice) => {
     choice.addEventListener("click", (e) => {
       let choiceInput = Array.from(choice.children).filter(
